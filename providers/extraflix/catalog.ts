1cd43954522c0b6d72604852e62d554e53dcbc5f
SADAB ALI <sadabgithub@gmail.com>
2025-11-26 22:36:57 +0530

    Add catalog and genres for movie categories

---
+++ providers/extraflix/catalog.ts
@@ -0,0 +1,44 @@
+export const catalog = [
+  {
+    title: "Latest Updates",
+    filter: "",
+  },
+  {
+    title: "Bollywood Movies",
+    filter: "/category/bollywood-movies",
+  },
+  {
+    title: "Hollywood (Hindi Dubbed)",
+    filter: "/category/hindi-dubbed-movies",
+  },
+  {
+    title: "South Indian (Hindi)",
+    filter: "/category/south-indian-hindi-dubbed-movies",
+  },
+  {
+    title: "Web Series",
+    filter: "/category/web-series",
+  },
+];
+
+export const genres = [
+  { title: "Action", filter: "/category/action-movies" },
+  { title: "Adventure", filter: "/category/adventure-movies" },
+  { title: "Animation", filter: "/category/animation-movies" },
+  { title: "Biography", filter: "/category/biography-movies" },
+  { title: "Comedy", filter: "/category/comedy-movies" },
+  { title: "Crime", filter: "/category/crime-movies" },
+  { title: "Detective", filter: "/category/detective-movies" },
+  { title: "Documentary", filter: "/category/documentary-movies" },
+  { title: "Drama", filter: "/category/drama-movies" },
+  { title: "Family", filter: "/category/family-movies" },
+  { title: "Fantasy", filter: "/category/fantasy-movies" },
+  { title: "History", filter: "/category/history-movies" },
+  { title: "Horror", filter: "/category/horror-movies" },
+  { title: "Musical", filter: "/category/musical-movies" },
+  { title: "Mystery", filter: "/category/mystery-movies" },
+  { title: "Romance", filter: "/category/romance-movies" },
+  { title: "Sci-Fi", filter: "/category/sci-fi-movies" },
+  { title: "Thriller", filter: "/category/thriller-movies" },
+  { title: "War", filter: "/category/war-movies" },
+];
