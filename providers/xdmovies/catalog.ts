a11d0cf6b36f950681a33c794303ac7de5332e02
SADAB ALI <sadabgithub@gmail.com>
2025-11-29 12:52:30 +0530

    Create catalog.ts

---
+++ providers/xdmovies/catalog.ts
@@ -0,0 +1,49 @@
+export const catalog = [
+  {
+    title: "Latest",
+    filter: "",
+  },
+  {
+    title: "Netflix",
+    filter: "/ott/netflix",
+  },
+  {
+    title: "Amazon Prime",
+    filter: "/ott/amazon",
+  },
+  {
+    title: "Disney+",
+    filter: "/ott/disney",
+  },
+  {
+    title: "Action",
+    filter: "/genre/action",
+  },
+  {
+    title: "Comedy",
+    filter: "/genre/comedy",
+  },
+  {
+    title: "Sci-Fi",
+    filter: "/genre/sci-fi",
+  },
+  {
+    title: "Thriller",
+    filter: "/genre/thriller",
+  },
+];
+
+export const genres = [
+  { title: "Action", filter: "/genre/action" },
+  { title: "Comedy", filter: "/genre/comedy" },
+  { title: "Drama", filter: "/genre/drama" },
+  { title: "Horror", filter: "/genre/horror" },
+  { title: "Sci-Fi", filter: "/genre/sci-fi" },
+  { title: "Thriller", filter: "/genre/thriller" },
+  { title: "Animation", filter: "/genre/animation" },
+  { title: "Crime", filter: "/genre/crime" },
+  { title: "Adventure", filter: "/genre/adventure" },
+  { title: "Mystery", filter: "/genre/mystery" },
+  { title: "Romance", filter: "/genre/romance" },
+  { title: "Fantasy", filter: "/genre/fantasy" },
+];
